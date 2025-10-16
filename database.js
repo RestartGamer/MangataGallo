2456232bd07465db42d9417d714a46ff98e56025
RestartGamer <ckorkmaz56@gmail.com>
2025-10-16 09:00:52 +0100

    makung sure everything is on the server

---
+++ database.js
@@ -0,0 +1,232 @@
+articles = [
+    {
+        id: "A0001",
+        category: "rings",
+        title: "Timeless Ring",
+        description: `Discover rings that capture emotion and elegance — from minimalist bands to bold statement pieces, each crafted to celebrate life’s most meaningful connections.`,
+        video: "video",
+        image: "media/rings-variant1.png",
+        imageSize: "100%",
+        imagePosition: "center center",
+        link: "link",
+        price: "1200€"
+    },
+
+    {
+        id: "A0002",
+        category: "necklaces",
+        title: "Elegant Necklace",
+        description: `Adorn your neckline with handcrafted designs that blend sophistication and charm. Each necklace tells a story of craftsmanship and personal expression.`,
+        video: "video",
+        image: "media/necklace-variant1.png",
+        imageSize: "100%",
+        imagePosition: "center center",
+        link: "link",
+        price: "1300€"
+    },
+
+    {
+        id: "A0003",
+        category: "earrings",
+        title: "Radiant Earring",
+        description: `From delicate studs to radiant drops, our earrings are designed to frame your features with elegance and individuality.`,
+        video: "video",
+        image: "media/earrings-special1.png",
+        imageSize: "100%",
+        imagePosition: "center center",
+        link: "link",
+        price: "960€"
+    },
+
+    {
+        id: "A0004",
+        category: "bracelets",
+        title: "Artistic Bracelet",
+        description: `Wrap your wrist in artistry — with bracelets that blend fine detail, balance, and personality for every mood and occasion.`,
+        video: "video",
+        image: "media/bracelet-variant1.jpg",
+        imageSize: "100%",
+        imagePosition: "center center",
+        link: "link",
+        price: "360€"
+    },
+
+    {
+        id: "A0005",
+        category: "watches",
+        title: "Refined Watch",
+        description: `More than an accessory, each watch is a symbol of refinement — merging precision, luxury, and timeless design in perfect harmony.`,
+        video: "video",
+        image: "media/watch-variant1.png",
+        imageSize: "100%",
+        imagePosition: "center center",
+        link: "link",
+        price: "2560€"
+    }
+];
+
+navbarContent = [
+    {
+        option: "Rings",
+        sections: [
+        {
+          section: "craft",
+          sectionName: "Craft",
+          listItems: [
+            "Engagement Rings for Every Love Story",
+            "Wedding Rings Crafted to Last",
+            "Fashion Rings to Express Your Unique Style"
+          ]
+        },
+        {
+          section: "moments",
+          sectionName: "Moments",
+          listItems: [
+            "Anniversary Rings to Celebrate Your Journey",
+            "Promise Rings That Speak From the Heart",
+            "Statement Rings for Bold Expressions",
+            "Remembrance Rings for the Best Moments"
+          ]
+        }
+      ]
+    },
+    {
+        option: "Necklaces",
+        sections: [
+        {
+          section: "design",
+          sectionName: "Design",
+          listItems: ["Pendants", "Chains", "Statement Necklaces"]
+        },
+        {
+          section: "inspire",
+          sectionName: "Inspire",
+          listItems: ["Layered Looks", "Chains", "Minimalist Styles"]
+        }
+      ]
+    },
+    {
+        option: "Earrings",
+        sections: [
+        {
+          section: "craft",
+          sectionName: "Craft",
+          listItems: ["Studs", "Hoops", "Drop Earrings"]
+        },
+        {
+          section: "stories",
+          sectionName: "Stories",
+          listItems: ["Everyday Essentials", "Statement Pieces"]
+        }
+      ]
+    },
+    {
+        option: "Bracelets",
+        sections: [
+        {
+          section: "craft",
+          sectionName: "Craft",
+          listItems: ["Bangles", "Chain-bracelets"]
+        },
+        {
+          section: "moments",
+          sectionName: "Moments",
+          listItems: ["Friendship", "Personalized"]
+        },
+        {
+          section: "ourluxurycollection",
+          sectionName: "Our Luxury Collection",
+          image: "media/bracelet-gold_2.jpg",
+          imageSize: "100%",
+          imagePosition: "center center"
+
+        },{
+          section: "craft",
+          sectionName: "Craft",
+          listItems: ["Bangles", "Chain-bracelets"]
+        },{
+          section: "craft",
+          sectionName: "Craft",
+          listItems: ["Bangles", "Chain-bracelets"]
+        },{
+          section: "craft",
+          sectionName: "Craft",
+          listItems: ["Bangles", "Chain-bracelets"]
+        },
+        {
+          section: "ourluxurycollection",
+          sectionName: "Our Luxury Collection",
+          image: "media/bracelet-gold_2.jpg",
+          imageSize: "100%",
+          imagePosition: "center center"
+
+        },
+      ]
+    },
+    {
+        option: "Watches",
+        sections: [
+        {
+          section: "design",
+          sectionName: "Design",
+          listItems: ["Luxury watches", "Fashion watches"]
+        },
+        {
+          section: "special",
+          sectionName: "Special Lines",
+          listItems: ["Modern Classics", "Heritage Styles"]
+        }
+      ]
+    },
+    {
+        option: "Collections",
+        sections: [
+        {
+          section: "stories",
+          sectionName: "Stories",
+          listItems: ["Seasonal Collections", "Limited Editions"]
+        },
+        {
+          section: "inspire",
+          sectionName: "Inspire",
+          listItems: ["Collaborations", "Designer Lines"]
+        }
+      ]
+    },
+    {
+        option: "Home",
+        sections: [
+            {
+                section: "none",
+                sectionName: "none",
+                listItems: "none"
+            }
+        ]
+    },
+    {
+        option: "Contact",
+        sections: [
+            {
+                section: "none",
+                sectionName: "none",
+                listItems: "none"
+            },
+            {
+                section: "inspire",
+                sectionName: "Inspire",
+                listItems: ["Collaborations", "Designer Lines"]
+            }
+        ]
+    },
+    {
+        option: "About",
+        sections: [
+            {
+                section: "title",
+                sectionName: "Who we are",
+                listItems: "This is the description of our company."
+            }
+        ]
+    },
+
+];
